d3ea687aea92bb916a5d47016702910b00fa2f08
Vincezamora-7 <vincezamora27@gmail.com>
2026-02-09 16:19:12 +0800

    Remove tracked files

--- src/hooks/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Custom Hooks Index
- * Central export point for all custom hooks
- */
-
-export { useScrollToSection } from './useScrollToSection';
-export { useFormHandler } from './useFormHandler';
--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Utilities Index
- * Central export point for all utility functions
- */
-
-export { logger } from './logger';
-export { validateEmail, validatePhoneNumber, validateUrl, validateFormData } from './validations';
